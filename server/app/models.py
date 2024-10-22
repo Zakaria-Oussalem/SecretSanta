28afa8c3e9439f0f3f5b10bb478833d3b33e6022
ZakariaOussalem <Zakaria.oussalem.pro@gmail.com>
2024-10-22 19:55:57 +0200

    make username non unique and fix table name users to user

--- server/app/models.py
+++ server/app/models.py
@@ -3,9 +3,9 @@ from app.database import Base
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "user"
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(20), unique=True, nullable=False)
+    username = Column(String(20), unique=False, nullable=False)
     role = Column(String(20))
     session = Column(Integer, nullable=False)
     attributed = Column(String(20))
